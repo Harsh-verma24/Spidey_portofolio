44564ca10c9b49a268666f22c5114126d353da5c
Harsh-verma24 <iamharshvrma@gmail.com>
2026-03-14 21:17:09 +0530

    navigations

--- app/components/home/Navbar.tsx
+++ app/components/home/Navbar.tsx
@@ -1,15 +1,23 @@
+"use client";
 import React from 'react'
 
 const Navbar = () => {
+    const scrollToSection = (id: string) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
         <div><nav className="fixed top-0 w-full z-50 backdrop-blur-md text-white/80">
             <div className="max-w-7xl mx-auto flex justify-center items-center px-6 py-4 ">
 
 
                 <ul className="flex gap-8 text-2xl tracking-wide font-sans font-medium select-none">
-                    <li className="hover:bg-gradient-to-r hover:from-[#e32832] hover:to-black hover:bg-clip-text hover:text-transparent cursor-pointer transition-all duration-300">Home</li>
-                    <li className="hover:bg-gradient-to-r hover:from-[#e32832] hover:to-black hover:bg-clip-text hover:text-transparent cursor-pointer transition-all duration-300">About</li>
-                    <li className="hover:bg-gradient-to-r hover:from-[#e32832] hover:to-black hover:bg-clip-text hover:text-transparent cursor-pointer transition-all duration-300">Contact</li>
+                    <li onClick={() => scrollToSection('home')} className="hover:bg-gradient-to-r hover:from-[#e32832] hover:to-black hover:bg-clip-text hover:text-transparent cursor-pointer transition-all duration-300">Home</li>
+                    <li onClick={() => scrollToSection('about')} className="hover:bg-gradient-to-r hover:from-[#e32832] hover:to-black hover:bg-clip-text hover:text-transparent cursor-pointer transition-all duration-300">About</li>
+                    <li onClick={() => scrollToSection('contact')} className="hover:bg-gradient-to-r hover:from-[#e32832] hover:to-black hover:bg-clip-text hover:text-transparent cursor-pointer transition-all duration-300">Contact</li>
                 </ul>
 
 
